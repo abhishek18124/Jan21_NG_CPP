e373e8a4ccccbee53a4701a8709c869f9a36e855
Abhishek Srivastava <abhishek@Abhisheks-MacBook-Pro-2.local>
2021-05-20 22:02:35 +0530

    initial commit

--- May/20May2021/live-session/Q3_0:1_KS.cpp
+++ May/20May2021/live-session/Q3_0:1_KS.cpp
@@ -15,11 +15,91 @@ Third line contains N space seperated integers where ith integer denotes the val
 */
 
 #include<iostream>
+#include<vector>
 
-using namespace  std;
+using namespace std;
+
+
+// int KSRecursive(vector<int>& prices, vector<int>& weights, int i, int w) {
+// 	// base case
+// 	if(i == prices.size() || w == 0) 
+// 		return 0;
+
+// 	// recursive case
+// 	if(weights[i] > w) {
+// 		// exclude the ith item 
+// 		return KSRecursive(prices, weights, i+1, w);
+// 	} 
+
+// 	int skip = KSRecursive(prices, weights, i+1, w);
+// 	int take = prices[i] + KSRecursive(prices, weights, i+1, w-weights[i]);
+// 	return max(skip, take);
+
+// }
+
+// int KSTopDown(vector<int>& prices, vector<int>& weights, vector<vector<int>>& memo, int i, int w) {
+// 	// base case
+// 	if(i == prices.size() || w == 0) 
+// 		return 0;
+
+// 	// lookup
+// 	if(memo[i][w] != -1) return memo[i][w];
+
+// 	// recursive case
+// 	if(weights[i] > w) {
+// 		// exclude the ith item 
+// 		return memo[i][w]=KSRecursive(prices, weights, i+1, w);
+// 	} 
+
+// 	int skip = KSRecursive(prices, weights, i+1, w);
+// 	int take = prices[i] + KSRecursive(prices, weights, i+1, w-weights[i]);
+// 	return memo[i][w]=max(skip, take);
+
+// }
+
 
 int main() {
 
-	
+	int n;
+	cin >> n;
+
+	int W;
+	cin >> W;
+
+	vector<int> weights(n);
+	for(int i=0; i<n; i++) {
+		cin >> weights[i];
+	}
+
+	vector<int> prices(n);
+	for(int i=0; i<n; i++) {
+		cin >> prices[i];
+	}
+
+	vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
+
+	// base case
+	for(int i=0; i<=n; i++) {
+		dp[i][0] = 0;
+	}
+
+	for(int j=0; j<=W; j++) {
+		dp[n][j] = 0;
+	}
+
+	// recursive
+	for(int i=n-1; i>=0; i--) {
+		for(int w=1; w<=W; w++) {
+			if(weights[i] > w) {
+				// I will skip the ith item
+				dp[i][w] = dp[i+1][w];
+			} else {
+				// I have an option to either skip or take the ith item
+				dp[i][w] = max(prices[i]+dp[i+1][w-weights[i]], dp[i+1][w]);
+			}
+		}
+	}
+
+	cout << dp[0][W] << endl;
 	return 0;
 }
