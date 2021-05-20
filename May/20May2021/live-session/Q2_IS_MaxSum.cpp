bc12a06f2159430742749a368721d747f69bd287
Abhishek Srivastava <abhishek@Abhisheks-MacBook-Pro-2.local>
2021-05-20 18:11:01 +0530

    initial commit

---
+++ May/20May2021/live-session/Q2_IS_MaxSum.cpp
@@ -0,0 +1,27 @@
+/*
+
+Given an integer array nums, return the increasing subsequence maximum sum.
+
+Example 1: 
+	Input : {1, 101, 2, 3, 100, 4, 5}
+	Output: 106 (1 + 2 + 3 + 100)
+
+Example 2:
+	Input : {3, 4, 5, 10} 
+	Output: 22 (3 + 4 + 5 + 10)
+
+Example 3:
+	Input : {10, 5, 4, 3}
+	Output: 10
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
---
+++ May/20May2021/live-session/Q3_0:1_KS.cpp
@@ -0,0 +1,25 @@
+/*
+
+You are packing for a vacation on the sea side and you are going to carry only one bag with capacity 
+S (1 <= S <= 1000). You also have N (1<= N <= 1000) items that you might want to take with you to the 
+sea side. Unfortunately you can not fit all of them in the knapsack so you will have to choose. 
+
+For each item you are given its size and its value. You want to maximize the total value of all the 
+items you are going to bring. What is this maximum total value?
+
+Input Format
+On the first line you are given N and S.
+Second line contains N space separated integer where ith integer denotes the size of ith item. 
+Third line contains N space seperated integers where ith integer denotes the value of ith item.
+
+*/
+
+#include<iostream>
+
+using namespace  std;
+
+int main() {
+
+	
+	return 0;
+}
